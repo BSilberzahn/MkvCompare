4d4557023b69109dce083755e1f873cfb9456060
BSilberzahn <benjamin.silberzahn@gmail.com>
2014-09-10 18:20:23 +0200

    Ajout du Controller

---
+++ MkvCompare/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MkvCompare
+{
+    class Controller
+    {
+        public static void test1()
+        {
+            Console.WriteLine("Test1");
+            Controller.test2();
+        }
+        private static void test2()
+        {
+            Console.WriteLine("Test2");
+        }
+    }
+}
