cc4629e684a9ea443d0b28116f8c8b7b3a5ec464
BSilberzahn <benjamin.silberzahn@gmail.com>
2014-09-10 19:53:29 +0200

    Ajout de la fonction pathToMkvList dans le Controller

--- MkvCompare/Controller.cs
+++ MkvCompare/Controller.cs
@@ -1,21 +1,46 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MkvCompare
 {
     class Controller
     {
-        public static void test1()
+        private static ArrayList movieList = new ArrayList();
+
+        public static ArrayList pathToMkvList(string path)
         {
-            Console.WriteLine("Test1");
-            Controller.test2();
+            return ListDirectory(new TreeView(), path);
         }
-        private static void test2()
+
+        private static ArrayList ListDirectory(TreeView treeView, string path)
         {
-            Console.WriteLine("Test2");
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+            return movieList;
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+                if (Path.GetExtension(file.Name) == ".mkv")
+                {
+                    movieList.Add(file.Name);
+                    //Console.WriteLine(file.Name);
+                }
+            }
+            return directoryNode;
         }
     }
 }
