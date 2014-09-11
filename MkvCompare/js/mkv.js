b61ca85f0d9702c42e49efd85087669516f2f602
CendreLight <GuildWarsLight@hotmail.fr>
2014-09-11 18:56:43 +0200

    ajout mkv.js et mkv.css

---
+++ MkvCompare/js/mkv.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('div#repository1').append('<p>Repo1</p>');
+	$('div#repository2').append('<p>Repo2</p>');
+	$('div#movie1To2').append('<p>Zone1</p>');
+	$('div#movie2To1').append('<p>Zone2</p>');
+    
+})
