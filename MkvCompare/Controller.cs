bab304ce734af19e754e2d9fe226dcbc2ee94db9
BSilberzahn <benjamin.silberzahn@gmail.com>
2014-09-25 13:20:49 +0200

    Ajout de la librairie NEbml et de quelques méthodes de lecture de tag

--- MkvCompare/Controller.cs
+++ MkvCompare/Controller.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NEbml.Core;
 
 namespace MkvCompare
 {
@@ -65,6 +66,130 @@ namespace MkvCompare
                 string otherFile = Path.Combine(otherPath, mkvFile.fullName);
                 File.Copy(file, otherFile);
             }
+        }
+
+        private static void getHiddenFiles(String path)
+        {
+            if (!System.IO.Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public static string GetMatroskaMovieDuration(string mkvFilePath)
+        {
+            MatroskaElementDescriptorProvider medp = new MatroskaElementDescriptorProvider();
+            using (var fs = new FileStream(mkvFilePath, FileMode.Open, FileAccess.Read))
+            using (EbmlReader ebmlReader = new EbmlReader(fs))
+            {
+                var segmentFound = ebmlReader.LocateElement(MatroskaElementDescriptorProvider.Segment);
+                if (segmentFound)
+                {
+                    ebmlReader.EnterContainer();
+                    while (ebmlReader.ReadNext())
+                    {
+                        var descriptor = medp.GetElementDescriptor(ebmlReader.ElementId);
+                        if (descriptor == null) continue;
+                        if (descriptor.Name == "Tracks")
+                        {
+                            Console.WriteLine("\t dans " + descriptor.Name);
+                            ebmlReader.EnterContainer();
+                            while (ebmlReader.ReadNext())
+                            {
+                                var trackDescriptor = medp.GetElementDescriptor(ebmlReader.ElementId);
+                                if (trackDescriptor == null) continue;
+                                if (trackDescriptor.Name == "TrackEntry")
+                                {
+                                    Console.WriteLine("\t dans " + trackDescriptor.Name);
+                                    ebmlReader.EnterContainer();
+                                    while (ebmlReader.ReadNext())
+                                    {
+                                        var trackEntryDescriptor = medp.GetElementDescriptor(ebmlReader.ElementId);
+                                        if (trackEntryDescriptor == null) continue;
+                                        if (trackEntryDescriptor.Name == "Video")
+                                        {
+                                            Console.WriteLine("\t dans " + trackEntryDescriptor.Name);
+                                            ebmlReader.EnterContainer();
+                                            while (ebmlReader.ReadNext())
+                                            {
+                                                var videoDescriptor = medp.GetElementDescriptor(ebmlReader.ElementId);
+                                                if (videoDescriptor == null) continue;
+                                                if (videoDescriptor.Name == "PixelWidth")
+                                                {
+                                                    Console.WriteLine("\t dans " + videoDescriptor.Name);
+                                                    Console.WriteLine("\t value " + ebmlReader.ReadUInt());
+                                                }
+                                                else if (videoDescriptor.Name == "PixelHeight")
+                                                {
+                                                    Console.WriteLine("\t value " + ebmlReader.ReadUInt());
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static List<string> GetMatroskaSubtitleLanguages(string mkvFilePath)
+        {
+            MatroskaElementDescriptorProvider medp = new MatroskaElementDescriptorProvider();
+            List<string> lstLanguages = new List<string>();
+
+            using (var fs = new FileStream(mkvFilePath, FileMode.Open, FileAccess.Read))
+            using (EbmlReader ebmlReader = new EbmlReader(fs))
+            {
+                var segmentFound = ebmlReader.LocateElement(MatroskaElementDescriptorProvider.Segment);
+                if (segmentFound)
+                {
+                    ebmlReader.EnterContainer();
+                    while (ebmlReader.ReadNext())
+                    {
+                        var descriptor = medp.GetElementDescriptor(ebmlReader.ElementId);
+                        if (descriptor == null) continue;
+                        if (descriptor.Name == "Tracks")
+                        {
+                            ebmlReader.EnterContainer();
+                            while (ebmlReader.ReadNext())
+                            {
+                                var trackDescriptor = medp.GetElementDescriptor(ebmlReader.ElementId);
+                                if (trackDescriptor == null) continue;
+                                if (trackDescriptor.Name == "TrackEntry")
+                                {
+                                    ebmlReader.EnterContainer();
+                                    long trackType = 0;
+                                    string trackLanguage = null;
+                                    while (ebmlReader.ReadNext())
+                                    {
+                                        var trackEntryDescriptor = medp.GetElementDescriptor(ebmlReader.ElementId);
+                                        if (trackEntryDescriptor == null) continue;
+                                        if (trackEntryDescriptor.Name == "TrackType")
+                                        {
+                                            trackType = ebmlReader.ReadInt();
+                                        }
+                                        else if (trackEntryDescriptor.Name == "Language")
+                                        {
+                                            trackLanguage = ebmlReader.ReadUtf();
+                                        }
+                                    }
+                                    if (trackType == 0x11) //subtitle
+                                    {
+                                        lstLanguages.Add(trackLanguage);
+                                    }
+                                    ebmlReader.LeaveContainer();
+                                }
+                            }
+                            ebmlReader.LeaveContainer();
+                            break;
+                        }
+                    }
+                }
+            }
+            return lstLanguages;
         }
     }
 }
